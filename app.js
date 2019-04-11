db7ef34aed5b85f7ce4639f2fa08f51b92902d2a
Abubakar Sambo <sadiqasg@gmail.com>
2019-04-11 12:21:49 +0100

    appjs(server-file): main file

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-import bodyParser from 'body-parser';
-import routes from './routes/index';
+const express = require('express');
+const bodyParser = require('body-parser');
 
-export default (app) => {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use('/api/v1', routes);
-};
+const routes = require('./routes/index');
+const port = process.env.PORT || 5000;
+const app = express();
+const apiversion1 = '/api/v1';
+
+
+app.get('/', (req, res) => {
+	res.json('Server is running.. Navigate to /api/v1');
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(`${apiversion1}`, routes);
+
+app.listen(port, () => console.log(`listening on port ${port}`))
