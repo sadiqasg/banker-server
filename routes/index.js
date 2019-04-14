6d17565e824e96915917f2733dacf9d97bc3345a
Abubakar Sambo <sadiqasg@gmail.com>
2019-04-14 12:34:28 +0100

    endpoint for login

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,11 @@ router.get('/', (req, res) => res.json('Successful!, Welcome to API v1!'));
 
 router.get('/users', User.getUsers);
 router.get('/users/:id', User.getOneUser);
-router.post('/users', User.createUser);
+router.post('/auth/users', User.createUser);
 router.patch('/users/:id', User.editUser);
 router.delete('/users/:id', User.deleteUser);
+
+router.post('/auth/signin', User.signIn);
 
 router.use('*', (req, res) => res.json('Route does not exist'));
 
